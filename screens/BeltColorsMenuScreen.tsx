925971bdc9fe3cbf349261a265165176c49f7ea2
amberhasan <amber.hasan@gmail.com>
2023-08-08 17:32:01 -0500

    Fixed UI of the belt colors screen and working on opening a screenpage when a color is clicked

--- screens/BeltColorsMenuScreen.tsx
+++ screens/BeltColorsMenuScreen.tsx
@@ -1,8 +1,10 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, StyleSheet, Alert, FlatList} from 'react-native';
+import {View, Alert, FlatList} from 'react-native';
 import axios from 'axios';
+import BeltColorMenuItem from '../components/BeltColorMenuItem'; // Update the path accordingly
+import WhiteBeltScreen from './belt_screens/WhiteBeltScreen'; // Adjust the path as needed
 
-const BeltColorsMenuScreen = () => {
+const BeltColorsMenuScreen = props => {
   const [belts, setBelts] = useState([]);
 
   useEffect(() => {
@@ -17,17 +19,49 @@ const BeltColorsMenuScreen = () => {
       });
   }, []);
 
+  const handleItemPress = item => {
+    // Implement navigation or any other action you want to perform on item press
+    // For navigation, you can use libraries like React Navigation
+    // For now, let's just log the item's color
+    console.log('Pressed item:', item.displayColor);
+    switch (item.id) {
+      case 1:
+        props.navigation.navigate('WhiteBeltScreen');
+        break;
+      case 2:
+        props.navigation.navigate('WhiteBeltScreen');
+        break;
+      case 3:
+        props.navigation.navigate('BeltRequirements');
+        break;
+      case 4:
+        props.navigation.navigate('BeltRequirements');
+        break;
+      case 5:
+        props.navigation.navigate('BeltRequirements');
+        break;
+      case 6:
+        props.navigation.navigate('BeltRequirements');
+        break;
+      case 7:
+        props.navigation.navigate('BeltRequirements');
+        break;
+      case 8:
+        props.navigation.navigate('BeltRequirements');
+        break;
+      case 9:
+        props.navigation.navigate('BeltRequirements');
+        break;
+    }
+  };
+
   return (
     <FlatList
       data={belts}
       keyExtractor={item => item.id}
-      renderItem={({item}) => {
-        return (
-          <View>
-            <Text>{item.displayColor}</Text>
-          </View>
-        );
-      }}
+      renderItem={({item}) => (
+        <BeltColorMenuItem item={item} onPress={() => handleItemPress(item)} />
+      )}
     />
   );
 };

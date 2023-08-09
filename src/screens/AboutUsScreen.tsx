e34c7ed66d4afb78ab1d4dc13d66d0f65aace3f8
amberhasan <amber.hasan@gmail.com>
2023-08-09 14:50:04 -0500

    Added collapsible component. It works

--- src/screens/AboutUsScreen.tsx
+++ src/screens/AboutUsScreen.tsx
@@ -1,10 +1,51 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
+import Collapsible from 'react-native-collapsible';
 
 const AboutUsScreen = () => {
+  const [locationCollapsed, setLocationCollapsed] = useState(true);
+  const [hoursCollapsed, setHoursCollapsed] = useState(true);
+  const [rulesCollapsed, setRulesCollapsed] = useState(true);
+  const [facebookCollapsed, setFacebookCollapsed] = useState(true);
+
+  const toggleLocation = () => setLocationCollapsed(!locationCollapsed);
+  const toggleHours = () => setHoursCollapsed(!hoursCollapsed);
+  const toggleRules = () => setRulesCollapsed(!rulesCollapsed);
+  const toggleFacebook = () => setFacebookCollapsed(!facebookCollapsed);
+
   return (
     <View>
-      <Text>AboutUsScreen</Text>
+      {/* Location */}
+      <TouchableOpacity onPress={toggleLocation}>
+        <Text>Location</Text>
+      </TouchableOpacity>
+      <Collapsible collapsed={locationCollapsed}>
+        <Text>Location details go here.</Text>
+      </Collapsible>
+
+      {/* Business Hours */}
+      <TouchableOpacity onPress={toggleHours}>
+        <Text>Business Hours</Text>
+      </TouchableOpacity>
+      <Collapsible collapsed={hoursCollapsed}>
+        <Text>Business hours details go here.</Text>
+      </Collapsible>
+
+      {/* School Rules */}
+      <TouchableOpacity onPress={toggleRules}>
+        <Text>School Rules</Text>
+      </TouchableOpacity>
+      <Collapsible collapsed={rulesCollapsed}>
+        <Text>School rules details go here.</Text>
+      </Collapsible>
+
+      {/* Facebook Page */}
+      <TouchableOpacity onPress={toggleFacebook}>
+        <Text>Facebook Page</Text>
+      </TouchableOpacity>
+      <Collapsible collapsed={facebookCollapsed}>
+        <Text>Facebook page details go here.</Text>
+      </Collapsible>
     </View>
   );
 };

7840161f6a1a508f3110918a1d0a30fd5d385eff
amberhasan <amber.hasan@gmail.com>
2023-08-14 08:25:43 -1000

    Google Maps works for Android

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import BottomNavigation from './BottomNavigation';
+import {enableLatestRenderer} from 'react-native-maps';
+
+enableLatestRenderer();
 
 const App = () => {
   return (

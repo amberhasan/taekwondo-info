93b91c9632ca139fa6c72fc9c9aff1b4a8c5208e
amberhasan <amber.hasan@gmail.com>
2023-08-23 13:40:50 -0500

    segmented button

--- src/screens/BeltDetailsScreen.tsx
+++ src/screens/BeltDetailsScreen.tsx
@@ -1,13 +1,68 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
-import {useRoute} from '@react-navigation/native';
+import {Appbar, Button} from 'react-native-paper';
 
 const BeltDetailScreen = () => {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const handleIndexChange = index => {
+    setSelectedIndex(index);
+  };
+
   return (
-    <View>
-      <Text>hello</Text>
+    <View style={styles.container}>
+      <View style={styles.segmentedControlContainer}>
+        <Button
+          mode={selectedIndex === 0 ? 'contained' : 'outlined'}
+          onPress={() => handleIndexChange(0)}
+          style={[styles.segmentedButton, styles.leftSegmentedButton]}>
+          Junior
+        </Button>
+        <Button
+          mode={selectedIndex === 1 ? 'contained' : 'outlined'}
+          onPress={() => handleIndexChange(1)}
+          style={[styles.segmentedButton, styles.rightSegmentedButton]}>
+          Senior
+        </Button>
+      </View>
+      <View style={styles.content}>
+        {selectedIndex === 0 ? (
+          <Text>Junior Content</Text>
+        ) : (
+          <Text>Senior Content</Text>
+        )}
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  segmentedControlContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 20,
+  },
+  segmentedButton: {
+    flex: 1,
+  },
+  leftSegmentedButton: {
+    borderTopRightRadius: 0,
+    borderBottomRightRadius: 0,
+    borderRightWidth: 0,
+  },
+  rightSegmentedButton: {
+    borderTopLeftRadius: 0,
+    borderBottomLeftRadius: 0,
+    borderLeftWidth: 0,
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default BeltDetailScreen;

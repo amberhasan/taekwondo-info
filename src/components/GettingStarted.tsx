de7cba3e2b7601d1f98b9e34325249946a9e31d1
amberhasan <amber.hasan@gmail.com>
2023-09-03 13:52:22 -0500

    Getting started screen

--- src/components/GettingStarted.tsx
+++ src/components/GettingStarted.tsx
@@ -8,16 +8,13 @@ import {
 } from 'react-native';
 // import ModalView from './ModalView';
 import colors from '../theme/colors';
+import ModalView from './ModalView';
 
 const GettingStarted = () => {
   const [modalVisible, setModalVisible] = useState(false);
 
   const handlePress = () => {
     setModalVisible(true);
-  };
-
-  const closeModal = () => {
-    setModalVisible(false);
   };
 
   return (
@@ -28,26 +25,6 @@ const GettingStarted = () => {
         margin: 10,
         overflow: 'hidden',
       }}>
-      <Modal visible={modalVisible} animationType="slide" transparent={true}>
-        <View
-          style={{
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-            backgroundColor: 'rgba(0, 0, 0, 0.5)',
-          }}>
-          {/* <ModalView style={{color: 'white', fontSize: 20}}>
-            <Text>
-              Try our $99 5-week Back To School Special!* It is a 5 week Back To
-              School Special for new students to try Taekwondo out! *Offer for
-              new students only
-            </Text>
-          </ModalView> */}
-          <TouchableOpacity onPress={closeModal} style={{marginTop: 20}}>
-            <Text style={{color: 'white', fontSize: 16}}>Close Modal</Text>
-          </TouchableOpacity>
-        </View>
-      </Modal>
       <TouchableOpacity onPress={handlePress}>
         <ImageBackground
           source={require('../assets/images/homepage/getting_started.png')}
@@ -82,6 +59,11 @@ const GettingStarted = () => {
           </View>
         </ImageBackground>
       </TouchableOpacity>
+      <ModalView isVisible={modalVisible} setIsVisible={setModalVisible}>
+        <View>
+          <Text>Hello</Text>
+        </View>
+      </ModalView>
     </View>
   );
 };

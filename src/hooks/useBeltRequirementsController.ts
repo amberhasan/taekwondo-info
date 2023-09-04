babe074de3163649609fd5ae7e61f612dd53c160
amberhasan <amber.hasan@gmail.com>
2023-09-04 10:28:52 -0500

    MVC for belt requirements

---
+++ src/hooks/useBeltRequirementsController.ts
@@ -0,0 +1,7 @@
+import {beltData} from '../models/beltRequirementsModel';
+
+const useBeltRequirementsController = () => {
+  return {beltData};
+};
+
+export default useBeltRequirementsController;
---
+++ src/models/beltRequirementsModel.ts
@@ -0,0 +1,52 @@
+export const beltData = [
+  {
+    name: 'White Belt',
+    id: 'white',
+    image: require('../assets/images/belts/white.png'),
+  },
+  {
+    name: 'White Yellow-Stripe Belt',
+    id: 'white_yellow_stripe',
+    image: require('../assets/images/belts/white_yellow_stripe.png'),
+  },
+  {
+    name: 'Yellow Belt',
+    id: 'yellow',
+    image: require('../assets/images/belts/yellow.png'),
+  },
+  {
+    name: 'Yellow Green-Stripe Belt',
+    id: 'yellow_green_stripe',
+    image: require('../assets/images/belts/yellow_green_stripe.png'),
+  },
+  {
+    name: 'Green Belt',
+    id: 'green',
+    image: require('../assets/images/belts/green.png'),
+  },
+  {
+    name: 'Green Blue-Stripe Belt',
+    id: 'green_blue_stripe',
+    image: require('../assets/images/belts/green_blue_stripe.png'),
+  },
+  {
+    name: 'Blue Belt',
+    id: 'blue',
+    image: require('../assets/images/belts/blue.png'),
+  },
+  {
+    name: 'Blue Red-Stripe Belt',
+    id: 'blue_red_stripe',
+    image: require('../assets/images/belts/blue_red_stripe.png'),
+  },
+  {
+    name: 'Red Belt',
+    id: 'red',
+    image: require('../assets/images/belts/red.png'),
+  },
+  {
+    name: '1st Dan Black Belt',
+    id: 'first_dan_evaluation',
+    image: require('../assets/images/belts/black.png'),
+  },
+];
